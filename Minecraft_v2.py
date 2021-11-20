05dd971f8ae9f0c8992393887d3bddb3c1efa105
Nithikrit Raibroycharoen <greninja028@Nithikrits-MacBook-Air.local>
2021-11-20 18:07:57 +0700

    Part 22

---
+++ Minecraft_v2.py
@@ -0,0 +1,463 @@
+from random import randrange
+from ursina import *
+from ursina.prefabs.first_person_controller import FirstPersonController
+from numpy import floor,abs,sin,cos,radians
+import time
+from perlin_noise import PerlinNoise
+from nMap import nMap
+from cave_system import Caves
+from tree_system import Trees
+from mining_system1 import Mining_system
+
+app = Ursina()
+
+# Our main character.
+subject = FirstPersonController()
+subject.cursor.visible = True
+subject.gravity = 0
+grav_speed = 0
+grav_acc = 0.1
+subject.x = subject.z = 5
+subject.y = 32
+prevZ = subject.z
+prevX = subject.x
+origin = subject.position # Vec3 object? .x .y .z
+
+# Load in texutre and model
+grassStokeTex = 'grass_14.png'
+monoTex = 'stroke_mono.png'
+stoneTex = 'grass_mono.png'
+
+cubeTex = 'block_texture.png'
+
+cubeModel = 'MoonCube'
+
+axoTex = 'axolotl.png'
+axoModel = 'axolotl.obj'
+
+axeModel = 'Diamond-Pickaxe'
+axeTex = 'diamond_axe_tex'
+
+# Important variables
+noise = PerlinNoise(octaves=1, seed=int(randrange(99,111)))
+seedMouth = Text(text='<white><bold>Your seed, today, sir, is ' +str(noise.seed), background=True)
+seedMouth.scale *= 1.4
+seedMouth.background.color = color.blue
+seedMouth.x = -0.52
+seedMouth.y = 0.4
+seedMouth.appear(speed=0.15)
+
+# print('seed is ' + str(noise.seed))
+
+megasets = []
+subsets = []
+subCubes = []
+# New variables :)
+generating = 1 # -1 if off.
+canGenerate = 1 # -1 if off.
+genSpeed = 0
+perCycle = 64
+currentCube = 0
+currentSubset = 0
+currentMegaset = 0
+numSubCubes = 64
+numSubsets = 10 # I.e. how many combined into a megaset?
+theta = 0
+rad = 0
+# Dictionary for recording whether terrain blocks exist
+# at location specified in key
+subDic = {}
+
+def createTerrainEntities():
+    # Intantiate our 'ghost' subset cubes.
+    for i in range(numSubCubes):
+        bud = Entity(model=cubeModel,texture=cubeTex)
+        bud.scale *= 0.99999
+        bud.rotation_y = random.randint(1,4)*90
+        bud.disable()
+        subCubes.append(bud)
+
+    # Instantiate our empty subset.
+    for i in range(numSubsets):
+        bud = Entity(model=cubeModel)
+        bud.texture = cubeTex
+        bud.disable()
+        subsets.append(bud)
+
+    # Instantiate our empty megasets.
+    for i in range(99):
+        bud = Entity(model=cubeModel)
+        bud.texture = cubeTex
+        bud.scale *= 0
+        bud.disable()
+        megasets.append(bud)
+
+createTerrainEntities()
+
+# save and load file functions :D
+def save():
+    global subDic, megasets, subsets, subCubes, noise
+    import pickle, os, sys
+    # Create a new entity that combiens all our
+    # subsets and megasets, which we can
+    # place onto a file.
+
+    # First, let's open/create a file in the
+    # folder we are working in (working directory)
+    # that we can save to
+    path = os.path.dirname(os.path.abspath(sys.argv[0]))
+    os.chdir(path)
+    with open('pickling.txt','wb') as f:
+        e = Entity()
+        for s in subsets:
+            if s.enabled == True:
+                s.parent = e
+        for m in megasets:
+            if m.enabled == True:
+                m.parent = e
+        e.combine(auto_destroy=False)
+        terrainModel = [    e.model.vertices,
+                            e.model.triangles,
+                            e.model.colors,
+                            e.model.uvs]
+        buildsModel = [     varch.builds.model.vertices,
+                            varch.builds.model.triangles,
+                            varch.builds.model.colors,
+                            varch.builds.model.uvs]
+        # Set parents to default
+        for s in subsets:
+            s.parent = scene
+        for m in megasets:
+            m.parent = scene
+        destroy(e)
+
+        newlist = [ subject.position,
+                    varch.tDic,
+                    subDic,
+                    terrainModel,
+                    noise,
+                    buildsModel,
+                    sol4r.treeslist
+                    ]
+        # Write game state objects to file.
+        pickle.dump(newlist, f)
+        # Clear out temporary lists.
+        newlist.clear()
+        terrainModel.clear()
+
+def load():
+    import pickle, sys, os
+    global subDic, rad, theta, currentSubset
+    global currentCube, currentMegaset, noise
+    global canGenerate, generating
+
+    # Open main module directory for correct file
+    path = os.path.dirname(os.path.abspath(sys.argv[0]))
+    os.chdir(path)
+
+    with open('pickling.txt', 'rb') as f:
+        nd = pickle.load(f)
+
+        # Populate our familiar terrain variables
+        # with data from the saved file
+
+        subject.position = copy(nd[0])
+        varch.tDic = copy(nd[1])
+        subDic = copy(nd[2])
+        tm = copy(nd[3])
+        noise = copy(nd[4])
+        bm = copy(nd[5])
+        entslist = copy(nd[6]) # Trees :)
+
+        # Now, let's delete all the current terrain
+        # And builds!
+        for s in subCubes:
+            destroy(s)
+        for s in subsets:
+            destroy(s)
+        for m in megasets:
+            destroy(m)
+        destroy(varch.builds)
+        subCubes.clear()
+        subsets.clear()
+        megasets.clear()
+        # Destroy all current trees in scene.
+        sol4r.treesCounter = 0
+        sol4r.treeslist.clear()
+        sol4r.trees.combine()
+        destroy(sol4r.trees)
+        sol4r.trees=Entity()
+        # Repopulate list of tree locations with
+        # Loaded data from file.
+        sol4r.treeslist = entslist
+
+        createTerrainEntities()
+        # Reset terrain generation variables.
+
+        # Go and plant loaded trees from the list
+        for t in entslist:
+            sol4r.plantTree(t[0],t[1],t[2])
+
+        megasets[0] = Entity(model=Mesh(
+                        vertices=tm[0],
+                        triangles=tm[1],
+                        colors=tm[2],
+                        uvs=tm[3]),
+                        texture=cubeTex)
+        # Bug solved! Note the texture needs to
+        # be 'varch.buildTex' for the builds model,
+        # not 'cubeTex'. The bug was caused by
+        # shining the builds colours through the
+        # terrain cube model.
+        varch.builds = Entity(model=Mesh(
+                        vertices=bm[0],
+                        triangles=bm[1],
+                        colors=bm[2],
+                        uvs=bm[3]),
+                        texture=varch.buildTex)
+
+        # Reset gamestate.
+        currentCube = 0
+        currentMegaset = 2 # NB different.
+        currentSubset = 0
+        rad = 0
+        theta = 0
+        generating = -1
+        canGenerate = -1
+
+# Our pickaxe :D
+pickaxe = Entity(model=axeModel,texture=axeTex,scale=0.07,position=subject.position,always_on_top=True)
+pickaxe.x -= 3
+pickaxe.z -= 2.2
+pickaxe.y -= subject.y
+pickaxe.rotation_z = 90
+pickaxe.rotation_y = 180
+
+pickaxe.parent=camera
+
+# Create a cave system object. It's called anush.
+anush = Caves()
+# Sam again, but gor trees :)
+sol4r = Trees()
+# And again, but for out mining system (built tools etc.).
+varch = Mining_system(subject, pickaxe, camera, subsets, megasets)
+
+# window.color = color.rgb(0,200,211)
+window.color = color.rgb(0,0,0)
+window.exit_button.visible = False
+
+prevTime = time.time()
+
+scene.fog_color = color.rgb(0,222,0)
+scene.fog_density = 0.02
+
+# load()
+
+def input(key):
+    global generating, canGenerate
+
+    # Deal with mining system's key inputs. Thanks.
+    varch.input(key)
+    # 'Smooth building' -- i.e. switch off
+    # terrain generation when in build mode automatically.
+    # On again if not in build mode (not automatic).
+    if varch.buildMode == 1:
+        generating = -1
+        canGenerate = -1
+    
+    if key == 'q' or key == 'escape':
+        quit()
+    if key == 'g': 
+        generating *= -1
+        canGenerate *= -1
+    
+    if key == 'b':
+        save()
+
+    if key == 'n':
+        load()
+
+# Main game loop :D
+def update():
+    global prevZ, prevX, prevTime, genSpeed, perCycle
+    global rad, origin, generating, canGenerate, theta
+    if abs(subject.z - prevZ) > 1 or \
+        abs(subject.x - prevX) > 1:
+        origin = subject.position
+        rad = 0
+        theta = 0
+        generating = 1 * canGenerate
+        prevZ = subject.z
+        prevX = subject.x
+
+            
+    generateShell()
+
+    if time.time() - prevTime > genSpeed:
+        for i in range(perCycle):
+            genTerrain()
+        prevTime = time.time()
+
+    vincent.look_at(subject, 'forward')
+    # vincent.rotation_x = 0
+
+    # Controls mining and building function
+    varch.buildTool()
+
+def genPerlin(_x, _z, plantTree=False):
+    y = 0
+    freq = 64
+    amp = 42
+    y += ((noise([_x/freq,_z/freq]))*amp)
+    freq = 32
+    amp = 21
+    y += ((noise([_x/freq,_z/freq]))*amp)
+
+    # Is there are cave-gap here?
+    # If so, lower the cube by 32...or something ;)
+    whatCaveHeight = anush.checkCave(_x, _z)
+    if whatCaveHeight != None:
+        y=whatCaveHeight
+    elif plantTree==True:
+        sol4r.checkTree(_x, y, _z)
+
+    return floor(y)
+
+def genTerrain():
+    global currentCube, theta, rad, currentSubset
+    global generating, currentMegaset
+
+    if generating == -1: return
+
+    # Decide where to place new terrain cube!
+    x = floor(origin.x + sin(radians(theta)) * rad)
+    z = floor(origin.x + cos(radians(theta)) * rad)
+    # Check whether there is terrain here already
+    if subDic.get('x' + str(x) + 'z' + str(z)) != 'i':
+        subCubes[currentCube].enable()
+        subCubes[currentCube].x = x
+        subCubes[currentCube].z = z
+        subCubes[currentCube].parent = subsets[currentSubset]
+        y = subCubes[currentCube].y = genPerlin(x,z,True)
+        # Record position of this terrain in both
+        # the subDic and the mining system's dictionary.
+        subDic['x' + str(x) + 'z'+str(z)] = 'i'
+        varch.tDic['x'+str(x)+'y'+str(y)+'z'+str(z)]=y
+        # OK -- time to decide colors :D
+        c = nMap(y, -8, 21, 132, 212)
+        c += random.randint(-32, 32)
+        subCubes[currentCube].color = color.rgb(c, c, c)
+        subCubes[currentCube].disable()
+        currentCube += 1
+
+        # Ready to build a subset?
+        if currentCube==numSubCubes:
+            subsets[currentSubset].combine(auto_destroy=False)
+            subsets[currentSubset].enable()
+            currentSubset+=1
+            currentCube=0
+            
+            # And ready to build a megaset?
+            if currentSubset==numSubsets:
+                # Parent all subsets to our new megaset.
+                for s in subsets:
+                    s.parent=megasets[currentMegaset]
+                # In case user has Ursina version 3.6.0.
+                # safe_combine(megasets[-1],auto_destroy=False)
+                megasets[currentMegaset].combine(auto_destroy=False)
+                for s in subsets:
+                    s.parent=scene
+                    # Invisible? Delete this line :)
+                    # s.disable()
+                currentSubset=0
+                currentMegaset+=1
+                print('Megaset #' + str(len(megasets))+'!')
+
+    else:
+        pass
+        # There was terrain already there, so
+        # continue rotation to find new terrain spot.
+    
+    if rad > 0:
+        theta += 45/rad
+    else: rad += 0.5
+
+    if theta >= 360:
+        theta = 0
+        rad += 0.5
+
+
+# Our new gravity system for moving the subject :)
+def generateShell():
+    global subject, grav_speed, grav_acc
+
+    # New 'new' system :D
+    # How high or low can we step/drop?
+    step_height = 3
+    gravityON = True
+    
+    target_y = subject.y
+
+    for i in range(step_height,-step_height,-1):
+        # What y is the terrain at this position?
+        # terra = genPerlin(subject.x,subject.z)
+        terra = varch.tDic.get( 'x'+str((floor(subject.x)))+
+                                'y'+str((floor(subject.y+i)))+
+                                'z'+str((floor(subject.z+0.5))))
+        terraTop = varch.tDic.get( 'x'+str((floor(subject.x)))+
+                                   'y'+str((floor(subject.y+i+1)))+
+                                   'z'+str((floor(subject.z+0.5))))
+        if terra != None and terra != 'gap':
+            gravityON = False
+            if terraTop == None or terraTop == 'gap':
+                # print('TERRAIN FOUND! ' + str(terra + 2))
+                target_y = floor(subject.y+i)# + 2
+                break
+            
+            # If here, then tower is too tall.
+            # So, move subject from this position.
+            subject.x -= 0.6
+            subject.z -= 0.6
+
+    if gravityON==True:
+        # This means we're falling!
+        grav_speed += (grav_acc * time.dt)
+        subject.y -= grav_speed
+    else:
+        subject.y = lerp(subject.y, target_y, 9.807*time.dt)
+        grav_speed = 0 # Reset gravity speed: gfloored.
+
+
+    # 'new' old system
+    """
+    # How high or low can we step/drop?
+    step_height = 5
+
+    # What y is the terrain at this position
+    target_y = genPerlin(subject.x, subject.z)
+    # Check if target is too high...
+    # How far are we from the target y?
+    target_dist = target_y - subject.y
+    #Can we step up or down?
+    if target_dist < step_height and target_dist > -step_height:
+        subject.y = lerp(subject.y, target_y, 9.807*time.dt)
+    elif target_dist < -step_height:
+        # This means we're falling!
+        grav_speed += grav_acc * time.dt
+        subject.y -= grav_speed
+    """
+
+    # global shellWidth
+    # for i in range(len(shellies)):
+    #     x = shellies[i].x = floor((i/shellWidth) + subject.x - 0.5*shellWidth)
+    #     z = shellies[i].z = floor((i%shellWidth) + subject.z - 0.5*shellWidth)
+    #     shellies[i].y = genPerlin(x,z)
+
+chickenModel = load_model('chicken.obj')
+vincent = Entity(model=chickenModel,scale=1,x=22,z=16,y=4,texture='chicken.png',double_sided=True)
+
+cyan_axolotl = Entity(model=axoModel,scale=10,x=-22,z=16,y=4,texture=axoTex,double_sided=True)
+
+generateShell()
+
+app.run()
---
+++ tree_system.py
@@ -0,0 +1,38 @@
+from perlin_noise import PerlinNoise
+from ursina import Entity, color, Vec3
+
+class Trees:
+    def __init__(self):
+        self.noise = PerlinNoise(seed=4)
+
+        # Parent of trees for optimization.
+        self.trees = Entity()
+        self.treesCounter = 0
+        # List of all trees planted
+        self.treeslist = []
+
+    def checkTree(self, _x, _y, _z):
+        freq = 5
+        amp = 100
+        TreeChance = ((self.noise([_x/freq,_z/freq]))*amp)
+        print(str(TreeChance))
+        if TreeChance > 30:
+            self.plantTree(_x, _y, _z)
+            # Record tree location to list of trees.
+            self.treeslist.append(Vec3(_x,_y,_z))
+
+    def plantTree(self, _x, _y, _z):
+        from random import randint
+        tree = Entity(model = None, position=Vec3(_x,_y,_z))
+        crown = Entity(model='cube',scale=6,y=7,color=color.green)
+        trunk = Entity(model='cube',scale_y=9,scale_x = 0.6,scale_z = 0.6,color=color.brown, collider='box')
+        crown.parent = tree
+        trunk.parent = tree
+        tree.y += 4
+        tree.rotation_y = randint(0,360)
+
+        tree.parent = self.trees
+        self.treesCounter += 1
+        if self.treesCounter % 4 == 0:
+            self.trees.combine()
+            self.trees.collider = self.trees.model
